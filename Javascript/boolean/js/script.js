cc795e09856519861ffc07e75c6c1b9a29c0686c
Taynara131705 <taynaralaura2017@gmail.com>
2024-08-22 11:09:49 -0300

    Add files via upload

---
+++ Javascript/boolean/js/script.js
@@ -0,0 +1,12 @@
+//Boolean verdadeiro e falso
+const vencedor = false;
+const perdedor = true;
+console.log(vencedor);
+console.log(typeof vencedor);
+console.log(perdedor);
+
+//Boolean com operação relacional
+
+const maioridade= 18;
+const maiordeIdade= maioridade > 17;
+console.log(maiordeIdade);
---
+++ Javascript/objetodoconsole/js/script.js
@@ -0,0 +1,19 @@
+//exibindo uma mensagem de aviso na console
+console.warn("Atenção: Aviso");
+
+//Exibindo uma mensagem de erro no console
+ console.error("Ocorreu erro");
+
+ //Exibindo uma  mensagem informativa no console
+ console.info("Informativa");
+ 
+
+ //Criando grupo de mensagem na console
+ console.group("Mensagem de Teste");
+ console.log("Atende-se ao passo 1");
+ console.log("Execute  o passo 2");
+ console.log("execute o passo final");
+ console.groupEnd();
+
+ //Limpar console
+ //console.clear();
